5deac0dce040a037399c716871b46167f098a927
Damion Uijldert <s1107777@student.hsleiden.nl>
2021-06-20 14:36:51 +0200

    package-lock.json

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -23,7 +23,7 @@ class AuthController extends Controller
             'password' => bcrypt($fields['password'])
         ]);
 
-        $token = $user->createToken('myapptoken')->plainTextToken;
+        $token = $user->createToken($request->name)->plainTextToken;
 
         $response = [
             'user' => $user,
@@ -49,7 +49,7 @@ class AuthController extends Controller
             ], 401);
         }
 
-        $token = $user->createToken('myapptoken')->plainTextToken;
+        $token = $user->createToken($request->name)->plainTextToken;
 
         $response = [
             'user' => $user,
--- routes/api.php
+++ routes/api.php
@@ -22,6 +22,10 @@ use Illuminate\Support\Facades\Route;
 //     Route::get('/products/search/{name}', [ProductController::class, 'search'];)
 // });
 
+Route::middleware('auth:sanctum')->get('/user', function (Request $requst) {
+    return $request->user();
+});
+
 Route::get('/register', [App\Http\Controllers\AuthController::class, 'register']);
 Route::get('/login', [App\Http\Controllers\AuthController::class, 'login']);
 
