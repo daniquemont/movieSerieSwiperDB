38c942de7dab8740b019284448933df86b697d2b
Damion Uijldert <s1107777@student.hsleiden.nl>
2021-06-21 22:16:52 +0200

    change code

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -6,101 +6,58 @@ use Illuminate\Http\Request;
 use Illuminate\Http\Response;
 use Illuminate\Support\Facades\Hash;
 use App\Models\User;
-use Validator;
 
 class AuthController extends Controller
 {
-    // public function register(Request $request){
-    //     $fields = $request->validate([
-    //         'name' => 'required|string',
-    //         'email' => 'required|string',
-    //         'password' => 'required|string|confirmed'
-    //     ]);
-
-    //     $user = User::create([
-    //         'name' => $fields['name'],
-    //         'email' => $fields['email'],
-    //         'password' => bcrypt($fields['password'])
-    //     ]);
-
-    //     $token = $user->createToken($request->name)->plainTextToken;
-
-    //     $response = [
-    //         'user' => $user,
-    //         'token' => $token
-    //     ];
-
-    //     return response($response, 201);
-        
-    // }
-
-    // public function login(Request $request){
-    //     $fields = $request->validate([
-    //         'email' => 'required|string|unique:users,email',
-    //         'password' => 'required|string|confirmed'
-    //     ]);
-
-    //     //check email
-    //     $user = User::where('email', $fields['email'])->first();
-
-    //     //check password
-    //     if(!$user || !Hash::check($fields['password'], $user->password)){
-    //         return response([
-    //             'message' => 'Bad creds'
-    //         ], 401);
-    //     }
-
-    //     $token = $user->createToken($request->name)->plainTextToken;
-
-    //     $response = [
-    //         'user' => $user,
-    //         'token' => $token
-    //     ];
-
-    //     // return response($response, 201);
-    //     return response()->json(User::all());
-    // }
-
-    public function register(Request $request)
-    {
-        $validator = Validator::make($request->all(), [
-            'name' => 'required',
-            'email' => 'required|email',
-            'password' => 'required',
+    public function register(Request $request){
+        $fields = $request->validate([
+            'name' => 'required|string',
+            'email' => 'required|string',
+            'password' => 'required|string|confirmed'
         ]);
-   
-        if($validator->fails()){
-            return $this->sendError('Validation Error.', $validator->errors());       
-        }
-   
-        $input = $request->all();
-        $input['password'] = bcrypt($input['password']);
-        $user = User::create($input);
-        $success['token'] =  $user->createToken('MyApp')->plainTextToken;
-        $success['name'] =  $user->name;
-   
-        return $this->sendResponse($success, 'User register successfully.');
-    }
-   
 
-    public function login(Request $request)
-    {
-        if(Auth::attempt(['email' => $request->email, 'password' => $request->password])){ 
-            $user = Auth::user(); 
-            $success['token'] =  $user->createToken('MyApp')->plainTextToken; 
-            $success['name'] =  $user->name;
-   
-            return $this->sendResponse($success, 'User login successfully.');
-        } 
-        else{ 
-            return $this->sendError('Unauthorised.', ['error'=>'Unauthorised']);
-        } 
-    }
-    public function logout(Request $request){
-        auth()->user()->tokens()->delete();
+        $user = User::create([
+            'name' => $fields['name'],
+            'email' => $fields['email'],
+            'password' => bcrypt($fields['password'])
+        ]);
 
-        return[
-            'message' => 'Logged out'
+        $token = $user->createToken($request->name)->plainTextToken;
+
+        $response = [
+            'user' => $user,
+            'token' => $token
         ];
+
+        return response($response, 201);
+        
     }
+
+    public function login(Request $request){
+        $fields = $request->validate([
+            'email' => 'required|string|unique:users,email',
+            'password' => 'required|string|confirmed'
+        ]);
+
+        //check email
+        $user = User::where('email', $fields['email'])->first();
+
+        //check password
+        if(!$user || !Hash::check($fields['password'], $user->password)){
+            return response([
+                'message' => 'Bad creds'
+            ], 401);
+        }
+
+        $token = $user->createToken($request->name)->plainTextToken;
+
+        $response = [
+            'user' => $user,
+            'token' => $token
+        ];
+
+        // return response($response, 201);
+        return response()->json(User::all());
+    }
+
 }
--- routes/api.php
+++ routes/api.php
@@ -26,9 +26,7 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $requst) {
     return $request->user();
 });
 
-
-
-Route::get('/register', [App\Http\Controllers\AuthController::class, 'register']);
+Route::post('/register', [App\Http\Controllers\AuthController::class, 'register']);
 Route::get('/login', [App\Http\Controllers\AuthController::class, 'login']);
 
 Route::get('/logout', [App\Http\Controllers\AuthController::class, 'logout']);
