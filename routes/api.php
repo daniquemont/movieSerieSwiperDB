235c8cd0cf5e348840173a9045e21b2bb581e60e
Damion Uijldert <s1107777@student.hsleiden.nl>
2021-06-22 11:09:29 +0200

    .

--- routes/api.php
+++ routes/api.php
@@ -25,11 +25,11 @@ use Illuminate\Support\Facades\Route;
 
 Route::post('/register', [App\Http\Controllers\AuthController::class, 'register']);
 Route::post('/login', [App\Http\Controllers\AuthController::class, 'login']);
+Route::post('/logout', [App\Http\Controllers\AuthController::class, 'logout']);
 
-
-Route::group(['middleware'=> ['auth:sanctum']], function(){
-    Route::post('/logout', [App\Http\Controllers\AuthController::class, 'logout']);
-});
+// Route::group(['middleware'=> ['auth:sanctum']], function(){
+    
+// });
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $requst) {
     return $request->user();
