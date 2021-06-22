46cfa14ae369c636641a7cba5e2c076b5bc728df
Damion Uijldert <s1107777@student.hsleiden.nl>
2021-06-22 18:42:29 +0200

    change web.php

--- routes/web.php
+++ routes/web.php
@@ -13,7 +13,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/register', [App\Http\Controllers\AuthController::class, 'register']);
 
 Route::get('/', function () {
     return view('welcome');
