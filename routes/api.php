c29053543eeff1500ba17bbd4eb0b075138a015a
Damion Uijldert <s1107777@student.hsleiden.nl>
2021-06-23 11:43:21 +0200

    change code

--- routes/api.php
+++ routes/api.php
@@ -14,7 +14,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::post('api/register', [App\Http\Controllers\AuthController::class, 'register']);
+Route::post('/register', [App\Http\Controllers\AuthController::class, 'register']);
 
 Route::post('/login', [App\Http\Controllers\AuthController::class, 'login']);
 
