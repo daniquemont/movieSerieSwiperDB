7be34dc519707bdecfb03625caa90ff22edf1b38
Damion Uijldert <s1107777@student.hsleiden.nl>
2021-06-23 11:54:11 +0200

    change register code

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -10,28 +10,33 @@ use App\Models\User;
 class AuthController extends Controller
 {
     public function register(Request $request){
-        $fields = $request->validate([
-            'name' => 'required|string',
-            'email' => 'required|string|email',
-            'password' => 'required|string',
-            'voorkeur' => 'string'
+        // $fields = $request->validate([
+        //     'name' => 'required|string',
+        //     'email' => 'required|string|email',
+        //     'password' => 'required|string',
+            
+        // ]);
+
+        // $user = User::create([
+        //     'name' => $fields['name'],
+        //     'email' => $fields['email'],
+        //     'password' => bcrypt($fields['password'])
+        // ]);
+
+        // $token = $user->createToken($request->name)->plainTextToken;
+
+        // $response = [
+        //     'user' => $user,
+        //     'token' => $token
+        // ];
+
+        // return response($response, 201);
+        return User::create([
+            'name' => $request->input('name'),
+            'email' => $request->input('email'),
+            'password' => Hash::make($request->input('password')),
+            'voorkeur' => 'actie'
         ]);
-
-        $user = User::create([
-            'name' => $fields['name'],
-            'email' => $fields['email'],
-            'password' => bcrypt($fields['password'])
-        ]);
-
-        $token = $user->createToken($request->name)->plainTextToken;
-
-        $response = [
-            'user' => $user,
-            'token' => $token
-        ];
-
-        return response($response, 201);
-        
     }
 
     public function login(Request $request){
