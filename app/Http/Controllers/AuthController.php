095d88122ce45791af664e87f7cf8fa72b9a9775
Damion Uijldert <s1107777@student.hsleiden.nl>
2021-06-23 14:45:50 +0200

    change register code

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -4,7 +4,8 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Illuminate\Http\Response;
-use Illuminate\Support\Facades\Hash;
+// use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Validator;
 use App\Models\User;
 
@@ -57,38 +58,63 @@ class AuthController extends Controller
     }
 
     public function login(Request $request){
-        $fields = $request->validate([
-            'email' => 'required|string|unique:users,email',
-            'password' => 'required|string|confirmed'
+        // $fields = $request->validate([
+        //     'email' => 'required|string|unique:users,email',
+        //     'password' => 'required|string|confirmed'
+        // ]);
+
+        // //check email
+        // $user = User::where('email', $fields['email'])->first();
+
+        // //check password
+        // if(!$user || !Hash::check($fields['password'], $user->password)){
+        //     return response([
+        //         'message' => 'Bad creds'
+        //     ], 401);
+        // }
+
+        // $token = $user->createToken($request->name)->plainTextToken;
+
+        // $response = [
+        //     'user' => $user,
+        //     'token' => $token
+        // ];
+
+        // // return response($response, 201);
+        // return response()->json(User::all());
+        
+        $validator = Validator::make($request-all(), [
+            'email' => 'required|email',
+            'password' => 'required',
         ]);
 
-        //check email
-        $user = User::where('email', $fields['email'])->first();
-
-        //check password
-        if(!$user || !Hash::check($fields['password'], $user->password)){
-            return response([
-                'message' => 'Bad creds'
-            ], 401);
+        if($validator->fails()){
+            return response()->json(['status_code' => 400, 'message' => 'Bad Request']);
         }
 
-        $token = $user->createToken($request->name)->plainTextToken;
+        $credentials = requst(['email', 'password']);
+        if(!Auth::attempt($credentials)){
+            return response()->json([
+                'status_code' => 500,
+                'message' => 'Unauthorized'
+            ]);
+        }
 
-        $response = [
-            'user' => $user,
-            'token' => $token
-        ];
+        $user = User::where('email', $request->email)->first();
 
-        // return response($response, 201);
-        return response()->json(User::all());
-
+        $tokenResult = $user->createToken('authToken')->plainTextToken;
+        return response()->json([
+            'status_code' => 200,
+            'message' => $tokenResult
+        ]);
     }
 
     public function logout(Request $request){
         $request->user()->currentAccessToken()->delete();
         
         return response()->json([
-            'status_code' => 200, 'message' => 'Token deleted successfully'
+            'status_code' => 200, 
+            'message' => 'Token deleted successfully'
         ]);
 
     }
